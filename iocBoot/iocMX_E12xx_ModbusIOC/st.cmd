757eccc8f9e60f79498dd4577e475f0b0bcc586d
Tomáš Mazanec <tomas.mazanec@eli-beams.eu>
2016-10-26 13:40:16 +0000

    initial commit (incl. all)

---
+++ iocBoot/iocMX_E12xx_ModbusIOC/st.cmd
@@ -0,0 +1,25 @@
+#!../../bin/linux-x86_64/MX_E12xx_ModbusIOC
+
+## You may have to change MX_E12xx_ModbusIOC to something else
+## everywhere it appears in this file
+
+< envPaths
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase "dbd/MX_E12xx_ModbusIOC.dbd"
+MX_E12xx_ModbusIOC_registerRecordDeviceDriver pdbbase
+
+cd ${TOP}/iocBoot/${IOC}
+
+< startup.script
+iocInit()
+
+# has to wait for a second !
+epicsThreadSleep 1
+< startup.variables
+
+dbl >MX_E12xx_ModbusIOC-PVs.txt
+
+#
